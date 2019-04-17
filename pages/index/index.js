c7297cdb497ebb02ae84c6d01245f01db49bb4c7
Kyra <kyrayang.5@gmail.com>
2019-04-17 13:20:38 +0800

    refresh

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,9 +1,38 @@
+const weatherMap = {
+  'sunny': '晴天',
+  'cloudy': '多云',
+  'overcast': '阴',
+  'lightrain': '小雨',
+  'heavyrain': '大雨',
+  'snow': '雪'
+}
+const weatherColorMap = {
+  'sunny': '#cbeefd',
+  'cloudy': '#deeef6',
+  'overcast': '#c6ced2',
+  'lightrain': '#bdd5e1',
+  'heavyrain': '#c5ccd0',
+  'snow': '#aae1fc'
+}
 Page({
+  data: {
+    nowTemp: '14',
+    nowWeather: 'Sunny',
+    bgUrl: '/image/sunny-bg.png'
+  },
   onLoad(){
+    this.getNow()
+  },
+  onPullDownRefresh(){
+    this.getNow(()=>{
+      wx.stopPullDownRefresh()
+    })
+  },
+  getNow(callback){
     wx.request({
-      url: 'https://test-miniprogram.com/api/weather/now?city=广州市',
+      url: 'https://test-miniprogram.com/api/weather/now?city=北京市',
       data: {
-       city:'广州市'
+       city:'北京市'
       },
       header: {
         'content-type': 'application/json'
@@ -12,12 +41,21 @@ Page({
         let result = res.data.result
         let temp = result.now.temp
         let weather = result.now.weather
-        return temp, weather
+        console.log(result),
+        this.setData({
+          nowTemp: temp +'°',
+          nowWeather: weatherMap[weather],
+          bgUrl:'/image/' + weather + '-bg.png',
+        })
+        wx.setNavigationBarColor({
+          frontColor: '#000000',
+          backgroundColor: weatherColorMap[weather],
+        })
       },
+      complete: ()=>{
+        callback && callback()
+      }
     })
   },
-  data: {
-    nowTemp: '14',
-    nowWeather: 'Sunny',
-  },
+  
 })
