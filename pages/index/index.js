b1f240674d14fb8b9e62294bb287a407eb99dce6
Kyra <kyrayang.5@gmail.com>
2019-04-17 11:05:26 +0800

    update

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,1 +1,23 @@
-Page({})
+Page({
+  onLoad(){
+    wx.request({
+      url: 'https://test-miniprogram.com/api/weather/now?city=广州市',
+      data: {
+       city:'广州市'
+      },
+      header: {
+        'content-type': 'application/json'
+      },
+      success: res =>{
+        let result = res.data.result
+        let temp = result.now.temp
+        let weather = result.now.weather
+        return temp, weather
+      },
+    })
+  },
+  data: {
+    nowTemp: '14',
+    nowWeather: 'Sunny',
+  },
+})
